89c2e4c492c4c1cbed689a74bbcf76e62333ddd8
JOAO DIVINO DA SILVA <60980853+joaodivinod@users.noreply.github.com>
2020-10-25 19:20:30 -0300

    Aula sobre one way data bind finalizada

--- minicurso/src/App.vue
+++ minicurso/src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
   <h1>Guia Clientes</h1>
-  <cliente></cliente>
-  <cliente></cliente>
-  <cliente></cliente>
+    <cliente></cliente>
+
 
   </div>
 </template>
 
 <script>
 
-import cliente from "./components/cliente";
+import cliente from "@/components/cliente";
+// import produto from "@/components/produto";
 
 export default {
   name: 'App',
   components:{
-    cliente
+    cliente,
+    // produto
   }
 
 }
